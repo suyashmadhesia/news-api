d713bcf970bf2bf6cd5e5158ccd8e4a92181d5b8
suyashmadhesia <suyashmadhesia@gmail.com>
2021-08-19 13:09:53 +0530

    server setup complete

--- apnamau/settings.py
+++ apnamau/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://apnamau.herokuapp.com/"]
 
 
 # Application definition
